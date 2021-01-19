8af327da8a3832f58d840f12232e01e93fe2fd60
keishaputritheanny <pkeisha47@gmail.com>
2021-01-19 17:59:18 +0700

    data input, Item adapter.kt, recipe.kt, mainActivity (card adapter), Internet Access

--- app/src/main/java/com/kei/recipeapp/MainActivity.kt
+++ app/src/main/java/com/kei/recipeapp/MainActivity.kt
@@ -2,11 +2,46 @@ package com.kei.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val list = ArrayList<Recipe>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+        rv_main.setHasFixedSize(true)
+        list.addAll(getRecipe())
+        showRecycler()
+    }
+
+    private fun showRecycler() {
+        rv_main.layoutManager = LinearLayoutManager(this)
+        val listRecipe = ItemAdapter(list)
+        rv_main.adapter = listRecipe
+    }
+    private fun getRecipe(): ArrayList<Recipe>{
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDesc = resources.getStringArray(R.array.data_description)
+        val dataIngredients = resources.getStringArray(R.array.data_ingredients)
+        val dataSteps = resources.getStringArray(R.array.data_steps)
+        val dataRating = resources.getIntArray(R.array.data_rating)
+        val dataPhoto = resources.getStringArray(R.array.data_photo)
+        val listRecipes = ArrayList<Recipe>()
+        for (position in dataName.indices){
+            val recipes = Recipe(
+                    dataName[position],
+                    dataDesc[position],
+                    dataIngredients[position],
+                    dataSteps[position],
+                    dataRating[position],
+                    dataPhoto[position]
+            )
+            listRecipes.add(recipes)
+        }
+        return listRecipes
     }
 }
